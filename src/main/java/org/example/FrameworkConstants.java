f2ad863c25cb0b614d9a007adebe5cbdbd2290ad
aleksey_tarasov <aleksei_tarasov@senla.eu>
2023-09-30 18:05:14 +0300

    add FrameworkConstants.java

---
+++ src/main/java/org/example/FrameworkConstants.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * FrameworkConstants has all constants that are used in the TestFramework.<br>
+ * To use constants use available getter methods.<br>
+ */
+public class FrameworkConstants {
+    /**
+     * private constructor
+     */
+    private FrameworkConstants() {
+    }
+
+    private static final String RESOURCES_PATH = System.getProperty("user.dir");
+    private static final String CONFIG_FILE_PATH = RESOURCES_PATH + "/src/test/resources/config/config.properties";
+}
