5ceda1314492fc20ea77a8e6c2902a65c1082472
aleksey_tarasov <aleksei_tarasov@senla.eu>
2023-09-30 18:06:23 +0300

    add getter for FrmeworkConstants.java

--- src/main/java/org/example/FrameworkConstants.java
+++ src/main/java/org/example/FrameworkConstants.java
@@ -13,4 +13,13 @@ public class FrameworkConstants {
 
     private static final String RESOURCES_PATH = System.getProperty("user.dir");
     private static final String CONFIG_FILE_PATH = RESOURCES_PATH + "/src/test/resources/config/config.properties";
+
+    /**
+     * Getter for property config path
+     *
+     * @return property config path
+     */
+    public static String getConfigfilepath() {
+        return CONFIG_FILE_PATH;
+    }
 }
