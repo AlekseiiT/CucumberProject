fdb71a871b744525db57057a1755aac98b4ae01a
aleksey_tarasov <coolerr195@gmail.com>
2023-10-07 22:23:09 +0300

    add ThreadLocal for UI proxy

---
+++ src/main/java/org/example/driver/ProxyManager.java
@@ -0,0 +1,45 @@
+package org.example.driver;
+
+import net.lightbody.bmp.BrowserMobProxy;
+import net.lightbody.bmp.BrowserMobProxyServer;
+import net.lightbody.bmp.proxy.CaptureType;
+
+/**
+ * Proxy manager is used for creating and using proxy in UI testing
+ */
+public class ProxyManager {
+    /**
+     * Private constructor
+     */
+    public ProxyManager(){}
+    /**
+     * ThreadLocal variable to deal with thread safety issues
+     */
+    private static final ThreadLocal<BrowserMobProxy> proxy = new ThreadLocal<>();
+
+    /**
+     * Getter for Proxy
+     * @return WebDriver instance
+     */
+    public static BrowserMobProxy getProxy() {
+        return proxy.get();
+    }
+
+    /**
+     * Setter for Proxy
+     */
+    public static void createProxy(){
+        BrowserMobProxyServer proxyServer = new BrowserMobProxyServer();
+        proxyServer.setTrustAllServers(true);
+        proxyServer.enableHarCaptureTypes(CaptureType.REQUEST_HEADERS, CaptureType.RESPONSE_HEADERS);
+        proxyServer.start(0);
+        proxy.set(proxyServer);
+    }
+
+    /**
+     * Unloads threadLocal variable
+     */
+    public static void unload(){
+        proxy.remove();
+    }
+}
