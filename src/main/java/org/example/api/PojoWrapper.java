5109861a667436eaff1f4c62cd67b38d988d55dc
aleksey_tarasov <coolerr195@gmail.com>
2023-10-08 11:24:35 +0300

    implement POJO wrapper class, and implement first mapping

---
+++ src/main/java/org/example/api/PojoWrapper.java
@@ -0,0 +1,28 @@
+package org.example.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+
+/**
+ * PojoWrapper class is used to create and work with structural data in form os POJO class
+ * Mainly used for API test scenarios
+ */
+public class PojoWrapper {
+
+    /**
+     * Private constructor
+     */
+    private PojoWrapper(){}
+
+    /**
+     * Generates pojo class out of jsonString
+     * @param jsonString
+     * @param clazz
+     * @return Object class should be cast manually after returning
+     */
+    @SneakyThrows
+    public static Object getPojoFromJsonString(String jsonString, Class clazz) {
+        return new ObjectMapper()
+                .readValue(jsonString, clazz);
+    }
+}
--- src/test/java/org/example/stepdefs/SubunitStepDef.java
+++ src/test/java/org/example/stepdefs/SubunitStepDef.java
@@ -4,6 +4,8 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
 import org.example.api.ApiManager;
+import org.example.api.PojoWrapper;
+import org.example.api.pojos.subunit.RootSubunit;
 import org.testng.Assert;
 
 public class SubunitStepDef {
@@ -17,6 +19,8 @@ public class SubunitStepDef {
     @Then("I check their values")
     public void iCheckTheirValues() {
         System.out.println(response.getBody().prettyPrint());
+
+        RootSubunit pojo = (RootSubunit) PojoWrapper.getPojoFromJsonString(response.asString(), RootSubunit.class);
         Assert.assertTrue(true);
     }
 }
