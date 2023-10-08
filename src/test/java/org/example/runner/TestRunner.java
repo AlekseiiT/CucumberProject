391f259897d20bd67afa0daad1745e831b93ac3f
aleksey_tarasov <coolerr195@gmail.com>
2023-10-08 22:06:25 +0300

    add CucumberOption plagin for adapter

--- src/test/java/org/example/runner/TestRunner.java
+++ src/test/java/org/example/runner/TestRunner.java
@@ -4,11 +4,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
-@CucumberOptions(features = "src/test/resources/features",
+@CucumberOptions(
+        features = "src/test/resources/features",
         glue = {"org.example.stepdefs"},
-        plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber.json"},
-        monochrome = true,
-        publish = true)
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        monochrome = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = false)
